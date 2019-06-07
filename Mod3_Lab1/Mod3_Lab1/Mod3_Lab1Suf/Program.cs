38b31d6a31648c8f16b0059c91f67ba0b5945c6e
DevJ5 <idevelopjs@gmail.com>
2019-06-07 08:22:11 +0200

    Streamreader

---
+++ Mod3_Lab1/Mod3_Lab1/Mod3_Lab1Suf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3_Lab1Suf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                string contents = streamReaderObject.ReadToEnd();
+
+                streamReaderObject.Close();
+
+                Console.WriteLine("The file has {0} text elements", new StringInfo(contents).LengthInTextElements);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file can not be found");
+            }
+            finally
+            {
+                if(streamReaderObject != null)
+                {
+                    streamReaderObject.Dispose();
+                }
+            }
+        }
+    }
+}
